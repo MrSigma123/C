fa25d460c02b1d7804bd46c5986591cdc1a8fd96
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-14 18:36:16 +0200

    Add Chapter5 5th question solution

--- CPrimerPlus/Chapter5/Questions/question5_5/question5_5.c
+++ CPrimerPlus/Chapter5/Questions/question5_5/question5_5.c
@@ -9,13 +9,19 @@ int main(void){
   printf("This program converts seconds to minutes and seconds.\n");
   printf("Enter the number of seconds, which you want to convert.\n");
   printf("Enter 0, to end the program.\n");
+  scanf("%d", &sec);   // adding second scanf() function call
   while (sec > 0) {
-    scanf("%d", &sec);
+    // scanf("%d", &sec);
     min = sec / SEC_IN_MIN;
     remainder = sec % SEC_IN_MIN;
     printf("%d seconds is %d minutes, %d seconds.\n", sec, min, remainder);
     printf("Next value?\n");
+    scanf("%d", &sec); // movind scanf() at the end of the while loop
   }
   printf("Goodbye!\n");
   return 0;
 }
+/* The problem here lays in uncertainty of the value stored initially inside sec variable.
+   If pre-initialized value is equal or smaller than 0 the program simply won't work.
+   To solve this problem there is required usage of another scanf() statement and moving
+   existent scanf() call at the end of the while loop. Fixed code is above.*/
