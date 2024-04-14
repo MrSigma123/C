ff9c4fa813061e6a0dbbbae94df8c4e02c79f2be
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-14 18:06:57 +0200

    Add Chapter5 4th question solution

--- CPrimerPlus/Chapter5/Questions/question5_4/question5_4.c
+++ CPrimerPlus/Chapter5/Questions/question5_4/question5_4.c
@@ -1,12 +1,13 @@
 // You suspect that the following program has some errors. Can you find them?
 #include <stdio.h>
 int main(void){
-  int i = 1,
+  int i = 1;  // lacking semicolon instead of comma character
   float n;
   printf("Warning! Incoming fractions!\n");
-  while (i < 30)
-    n = 1/i;
-    printf(" %f", n);
-  printf("That's all.\n");
+  while (i < 30){ // lacking curly brackets
+    n = (float)1/i++;    // lacking incrementation and castiong operator
+    printf("%f\n", n);   // added \n for iproved readability
+  }
+  printf("That's all for now.\n");
   return 0;
 }
