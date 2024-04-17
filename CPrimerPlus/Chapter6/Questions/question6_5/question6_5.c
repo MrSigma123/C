2de356c2b9ce27d53c9f0c7154390b6389e900d5
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-17 14:50:34 +0200

    Add Chapter6 5th question solution

--- CPrimerPlus/Chapter6/Questions/question6_5/question6_5.c
+++ CPrimerPlus/Chapter6/Questions/question6_5/question6_5.c
@@ -2,12 +2,14 @@
   you are able to find in the source code? */
 #include <stdio.h>
 int main(void) {
-  int i, j, lista(10);          // curly brackets instead square
+  int i, j, list[10];          // curly brackets instead square and fixed typo
 
-  for (i = 1, i <= 10, i++) {   // wrong separators, comma instead semicolon, 0..9 indexes instead
-    list[i] = 2*i + 3;          // fine so far
-    for (j = 1, j > = i, j++)   // pain to the eyes, improper <:= operator typing and wrong separators
+  for (i = 0; i <= 9; i++) {   // wrong separators, comma instead semicolon, 0..9 indexes instead
+    list[i] = 2*i + 3;
+    for (j = 0; j <= i; j++)   // pain to the eyes, improper <:= operator typing and wrong separators
       printf(" %d", list[j]);
     printf("\n");
+  }
+  return 0;
   // lost closing curly bracket and return statement
 }
