4fe8c6e449445faabb425bc0cea6ce4bde6b2fd8
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-17 14:54:21 +0200

    Add Chapter6 5th question solution

--- CPrimerPlus/Chapter6/Questions/question6_5/question6_5.c
+++ CPrimerPlus/Chapter6/Questions/question6_5/question6_5.c
@@ -1,15 +1,13 @@
-/* You suspest the following program to be faulty. What errors
-  you are able to find in the source code? */
+/* Fixed code */
 #include <stdio.h>
 int main(void) {
-  int i, j, list[10];          // curly brackets instead square and fixed typo
+  int i, j, list[10];
 
-  for (i = 0; i <= 9; i++) {   // wrong separators, comma instead semicolon, 0..9 indexes instead
+  for (i = 0; i <= 9; i++) {   
     list[i] = 2*i + 3;
-    for (j = 0; j <= i; j++)   // pain to the eyes, improper <:= operator typing and wrong separators
+    for (j = 0; j <= i; j++)  
       printf(" %d", list[j]);
     printf("\n");
   }
   return 0;
-  // lost closing curly bracket and return statement
-}
+ }
