9f976786b434097265354cc5d843479473def26c
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-20 23:50:25 +0200

    Add Chapter7 1st question solution

--- CPrimerPlus/Chapter7/Questions/question7_1/question7_1.c
+++ CPrimerPlus/Chapter7/Questions/question7_1/question7_1.c
@@ -1,6 +1,6 @@
 /* Specify which expressions are true and which are false */
 #include <stdio.h>
-int main(void) {  // Applying consistent indentation (K & R derivative)
+int main(void) {
   printf("a) 100 > 3 && 'a' > 'c'; A: 0, R: %d\n", 100 > 3 && 'a' > 'c');
   printf("b) 100 > 3 || 'a' > 'c'; A: 1, R: %d\n", 100 > 3 || 'a' > 'c');
   printf("c) !(100 > 3); A: 0, R: %d\n", !(100 > 3));
