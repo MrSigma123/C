a3f26b2891ddaf2d55387e36f210a5ace20a07ad
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-21 17:52:20 +0200

    Add Chapter7 7th question solution

--- CPrimerPlus/Chapter7/Questions/question7_7/question7_7.c
+++ CPrimerPlus/Chapter7/Questions/question7_7/question7_7.c
@@ -2,17 +2,18 @@
 #include <stdio.h>
 int main(void) {
   char ch;
-  int lc_count = 0;     /* lowercase counter
-  int capt_count = 0;   /* capital counter
-  int other_count = 0;  /* other character counter
-  while ((ch = getchat()) != '#') {
-    if ('a' <= ch >= 'z')
+  int lc_count = 0;     /* lowercase counter */ // close comments
+  int capt_count = 0;   /* capital counter   */
+  int other_count = 0;  /* other character counter */
+  while ((ch = getchar()) != '#') {       // fix typo
+    if ('a' <= ch && ch <= 'z')           // fix condition statement
       lc_count++;
-    else if (!(ch < 'A') || !(ch > 'Z'))
+    else if (!(ch < 'A') && !(ch > 'Z'))  // fix another condition
       capt_count++;
-    other_count++;
+    else                                  // add else statement
+      other_count++;
   }
-  printf(%d lowercase letters, %d capital letters, %d other characters, lc_count,
-      capt_count, other_count);
+  printf("%d lowercase letters, %d capital letters, %d other characters", lc_count,
+      capt_count, other_count);           // add quotation marks
   return 0;
 }
