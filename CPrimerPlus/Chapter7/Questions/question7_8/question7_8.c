23052e31ed8c196cac444c7a7d90f254ef5f5398
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-21 18:07:22 +0200

    Add Chapter7 8th question solution

--- CPrimerPlus/Chapter7/Questions/question7_8/question7_8.c
+++ CPrimerPlus/Chapter7/Questions/question7_8/question7_8.c
@@ -5,8 +5,8 @@ int main(void) {
   while (age++ <= 65) {
     if ((age % 20) == 0)  /* is age multiple of 20 ? */
       printf("You are %d years old. You are getting a raise.\n", age);
-    if (age = 65)
+    if (age == 65)  // replace assignment operator with equality operator
       printf("You are %d years old. You are getting brand new gold watch.\n", age);
-  }
+  } // Unfixed program displays infinite loop
   return 0;
 }
