4babec8f67274a2c61eb83973191f57f3169ffc2
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-21 19:00:12 +0200

    Add Chapter7 10th question solution

--- CPrimerPlus/Chapter7/Questions/question7_10/question7_10.c
+++ CPrimerPlus/Chapter7/Questions/question7_10/question7_10.c
@@ -5,32 +5,17 @@ int main(void) {
   char ch;
   printf("After qchc the program will display:\n");
   printf("Step 1\nStep 2\nStep 3\nStep 1\nStep 1\nStep 3\nStep 1\nEnd\n");
-  while ((ch = getchar()) != '#') {
-    while (ch == '\n')
-      ch = getchar();
-    printf("Step 1\n");
-    if (ch == 'c') {
-      ch = getchar();
+  while ((ch = getchar()) != '#') { // make more optimized and neat
+    if (ch != '\n') {               // remove multiple getchar() calls
       printf("Step 1\n");
+      if (ch == 'b')
+        break;
+      else if (ch != 'c') {
+        if (ch != 'h')
+          printf("Step 2\n");
+        printf("Step 3\n");
+      }
     }
-    else if (ch == 'b')
-      break;
-    else if (ch == 'h') {
-      printf("Step 3\n");
-      ch = getchar();
-      while (ch == '\n')
-      ch = getchar();
-    printf("Step 1\n");
-    if (ch == 'c') {
-      ch = getchar();
-      printf("Step 1\n");
-    }
-    else if (ch == 'b')
-      break;
-    }
-    if (ch != 'h')
-      printf("Step 2\n");
-  printf("Step 3\n");
   }
   printf("End\n");
   return 0;
