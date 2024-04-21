85e980295c6ed6dcb2de41c5c27b132555309cdb
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-21 22:20:47 +0200

    Add Chapter7 4th exercise solution

---
+++ CPrimerPlus/Chapter7/Exercises/exercise7_4/exercise7_4.c
@@ -0,0 +1,29 @@
+/* Using if else statement write a program which reads input data
+  up to # character and then replaces every single dot character
+  with en exclamation mark and every exclamation mark with two
+  exclamation marks. The program should also display the number
+  of changes the program has done. */
+#include <stdio.h>
+#define DOT '.'
+#define EXCLAMATION_MARK '!'
+int main(void) {
+  char character;
+  int changes = 0;
+  printf("This program replaces dots with exclamation marks and\n");
+  printf("replaces exclamation marks with two exclamation marks.\n");
+  printf("Enter the tream of input to be modified (# ends the input):\n");
+  while (scanf("%c", &character) && character != '#') {
+    if (character == '.') {
+      character = '!';
+      changes++;
+    }
+    else if (character == '!') {
+      putchar(character);
+      changes++;
+    }
+    putchar(character);
+  }
+  printf("The program has done %d changes.\n", changes);
+  return 0;
+}
+
