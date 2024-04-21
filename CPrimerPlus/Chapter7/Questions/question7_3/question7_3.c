af3d464820e974753fe1933f83f1bedc86f42751
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-21 16:48:38 +0200

    Add Chapter7 3rd question solution

--- CPrimerPlus/Chapter7/Questions/question7_3/question7_3.c
+++ CPrimerPlus/Chapter7/Questions/question7_3/question7_3.c
@@ -3,17 +3,19 @@
 #include <stdio.h>
 int main(void) {
   int weight, height;
-
-  scanf("%d", weight, height);
-  if (weight < 45 && height > 135)
+  printf("Enter your weight (kg) and height (cm): ");
+  scanf(" %d %d", &weight, &height);            // add format specifier and & operator
+  if (weight < 45 && height > 135){             // add opening bracket
     if (height >= 182)
       printf("You are very tall for your weight.\n");
-    else if (height < 182 && > 163)
+    else if (height < 182 && height > 163)      // add missing height variable
       printf("You are tall for your weight.\n");
-  else if (weight > 135 && ! (weight <= 135)
-           && height < 122)
-    if (!(height >= 122))
-      printf("You're quite short as for your weight.n\n");
+    else                                        // create additional else statement
+      printf("You are probably still groiwng!\n");
+  }                                             // add closing bracket
+  else if (weight > 135 && height < 122)        // removed unnecessary duplicate expression
+    // if (!(height >= 122))                    // remove unnecesary condition
+    printf("You're quite short as for your weight.n\n");
   else
     printf("Your weight is perfect.\n");
 
