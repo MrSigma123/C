a9ad189a6338590037a5e6111f8b58b5c8ec55c8
MrSigma123 <karol.czolpinski@gmail.com>
2024-04-25 23:44:09 +0200

    Add Chapter8 5th question solution

---
+++ CPrimerPlus/Chapter8/Questions/question8_5/question8_5.c
@@ -0,0 +1,8 @@
+/* What is EOF? */
+#include <stdio.h>
+int main(void) {
+  printf("EOF is the macro which stores the system representation of \"End of line\"\n"
+         "charcter which indicates that there is no next character stored in the file.\n");
+  printf("Functions line scanf() and getchar() can use this value to stop reading the dara stream.\n");
+  return 0;
+}
