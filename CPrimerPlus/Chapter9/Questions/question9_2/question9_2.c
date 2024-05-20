152d51cc0e3973b28002b52af23976da04b5596a
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-20 23:53:16 +0200

    Add Chapter9 2nd question problem

---
+++ CPrimerPlus/Chapter9/Questions/question9_2/question9_2.c
@@ -0,0 +1,15 @@
+/* Write function prototypes (in ANSI style) according to the
+  function descriptions given below (omit the function definition)
+  
+  a) function package() displays the number of zeroes specified by
+    passed argument of the type int
+  b) function run() which takes two arguments of the int type and
+    returns int type value
+  c) function guess() which doesn't take any arguments and returns
+    the int type value
+  d) function floor() which takes the variable of the type double
+    and this variable address, then the function writes passed
+    variable under given address
+*/
+
+
