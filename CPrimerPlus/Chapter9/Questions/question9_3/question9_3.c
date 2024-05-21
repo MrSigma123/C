0ea18f54ca749b7b4685814dc1885950c73add04
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-21 23:55:05 +0200

    Add Chapter9 3rd question problem

---
+++ CPrimerPlus/Chapter9/Questions/question9_3/question9_3.c
@@ -0,0 +1,12 @@
+/* Write function prorotypes (in ANSI style) according to the following
+  function descriptions (omit the function definition):
+
+  a) function n_char() which takes one int type argument and returns
+    one char type argument
+  b) function digits() which takes one double type argument and one
+    int type argument and returns one int type value
+  c) function which() which takes two addresses of the double type
+    and returns one double type address
+  d) function random() which doesn't take any arguments and returns
+    one int type value */
+
