ce433269098c0f43a2f7e9777cbdffd6295bac80
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-23 19:58:21 +0200

    Add Chapter9 9th question solution

--- CPrimerPlus/Chapter9/Questions/question9_9/question9_9.c
+++ CPrimerPlus/Chapter9/Questions/question9_9/question9_9.c
@@ -8,9 +8,10 @@
   a) Write a function which displays the menu with
   four options (it should look like like above)
 
-  b) Write a function which takes two arguments
-  of the int type and should take from the input one
-  integer value. If the value doesn't fit into the range
+  b) Write a function which takes two arguments,
+  the lower and the upper limit of the range,
+  their type should be int. The function should take from the input
+  one integer value. If the value doesn't fit into the range
   the function should display again the menu (using menu
   function from a) and take the new value. In case of entering
   the number in range the function should return it to the calling
@@ -22,16 +23,42 @@
   menu. The program should just display all options and take the value
   entered by the user.
   */
+
 #include <stdio.h>
 
 void menu(void);
+int choice(int lowLim, int highLim);
 
 int main(void) {
+  int low, high, value;
   menu();
+  printf("Enter the lower limit: ");
+  scanf("%d", &low);
+  printf("Enter the upper limit: ");
+  scanf("%d", &high);
+  value = choice(low, high);
+  printf("The returned value is %d.\n", value);
+  return 0;
 }
+
 void menu (void) {
-  printf("Choose one of the following options:\n");
+  printf("\nChoose one of the following options:\n");
   printf("1) copying files        2) transfering files\n");
   printf("3) removing files       4) end\n");
-  printf("Enter the number corresponding to desired option: \n");
+  printf("Enter the number corresponding to desired option:\n");
+}
+
+int choice (int lowLim, int highLim) {
+  int test_value;
+  printf("\nEnter the value to check if it fits in range: ");
+  while (scanf("%d", &test_value) == 1) {
+    if (test_value >= lowLim && test_value <= highLim) {
+      return test_value;
+    }
+    else {
+      menu();
+      printf("\nEnter the value to check if it fits in range: ");
+    }
+  }
+  return 4; // for non numerical test_value
 }
