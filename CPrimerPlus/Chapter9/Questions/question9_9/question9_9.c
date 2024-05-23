f4ea3a0d50bf2bf80c16059365684f5f64d0d163
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-23 19:28:40 +0200

    Add Chapter9 9th question problem

---
+++ CPrimerPlus/Chapter9/Questions/question9_9/question9_9.c
@@ -0,0 +1,24 @@
+/* Assuming the following output data:
+  
+  Choose one of the following options:
+  1) copying files        2) transfering files
+  3) removing files       4) end
+  Enter the number corresponding to desired option: 
+
+  a) Write a function which displays the menu with
+  four options (it should look like like above)
+
+  b) Write a function which takes two arguments
+  of the int type and should take from the input one
+  integer value. If the value doesn't fit into the range
+  the function should display again the menu (using menu
+  function from a) and take the new value. In case of entering
+  the number in range the function should return it to the calling
+  function. Entering non digit value should case returning 4
+
+  c) Write dummy program which uses the functions from a) and b)
+  from the current question. The dummy program means that the program
+  doesn't truely have to perform all the tasks which are mentioned in
+  menu. The program should just display all options and take the value
+  entered by the user.
+  */
