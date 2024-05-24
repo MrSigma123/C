bfca6cabd4179de7447883e0afffb8eb584a41b1
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-24 11:33:51 +0200

    Add Chapter9 7th exercise problem

---
+++ CPrimerPlus/Chapter9/Exercises/exercise9_7/exercise9_7.c
@@ -0,0 +1,9 @@
+/* Write a program which reads the characters from stdin stream untill
+  EOF is detected. For each character the program should inform if that
+  certain character is a letter. If that character is a letter than the
+  program should display the number of this letter in the latin alphabet.
+  In example for the letter c and C both of these letters have the number
+  3. Use a function which takes the caracter as an argument and returns
+  the number of it's order in latin alphabet if it's the letter. In other
+  case the function should return -1. */
+
