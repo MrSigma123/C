2177a2bb4126321681198961b66dbfc8da05a9e2
MrSigma123 <karol.czolpinski@gmail.com>
2024-05-26 16:49:30 +0200

    Add Chapter9 10th exercise problem

---
+++ CPrimerPlus/Chapter9/Exercises/exercise9_10/exercise9_10.c
@@ -0,0 +1,8 @@
+/* Generalize the function to_binary() from listing 9.8 to the to_base_n()
+  which should take as the second argument the value from the rage (2 - 10).
+  Then that function should display the number taken as the first argument
+  which is corresponding to the second argument represented in other number
+  system. In example the function to_base_n(129, 8) should display 201 which
+  is the octal representation of 129 in decimal system. Check if that function
+  works correctly by writing simple program which uses that function. */
+
