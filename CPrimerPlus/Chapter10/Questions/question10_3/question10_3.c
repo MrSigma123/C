22638b22623f47e7518c38a911a21f892d1a6d90
MrSigma123 <karol.czolpinski@gmail.com>
2024-07-11 21:14:24 +0200

    Add Chapter10 3rd question solution

---
+++ CPrimerPlus/Chapter10/Questions/question10_3/question10_3.c
@@ -0,0 +1,8 @@
+// What's the addres ref in question10_1 ? And ref + 1? What it points to?
+#include <stdio.h>
+int main(void) {
+  printf("The ref address is the address of the first element of ref[] array.\n");
+  printf("The address of an array + any decimal number reffers to specific array\n");
+  printf("element. This notation is called \"pointer arithmetic.\"\n");
+  return 0;
+}
