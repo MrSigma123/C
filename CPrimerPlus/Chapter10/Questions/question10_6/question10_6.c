0cb342513d2f7d527b9ab1c28237cb407d5ce899
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-18 00:19:50 +0200

    Add Chapter10 6th question solution

--- CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
+++ CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
@@ -4,3 +4,17 @@
 // a) express the address of the element net[22][56]
 // b) express the address of the element net[22][0] in two different ways
 // c) express the address of the element net[0][0] in three different ways
+#include <stdio.h>
+int main(void) {
+  int net[30][100];
+  int * p1;
+  p1 = &net[22][56];
+  int * p2;
+  p2 = &net[22][0];
+  p2 = net[22];
+  int * p3;
+  p3 = &net[0][0];
+  p3 = net[0];
+  p3 = *net;
+  return 0;
+}
