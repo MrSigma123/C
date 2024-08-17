6538e8f4083d4a7b4106dc25eb516f4c2fe56bac
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-18 00:01:56 +0200

    Update question10_6 question problem

--- CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
+++ CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
@@ -1,4 +1,6 @@
 // question10_6
 // Let's assume that we have the following declaration
 //   int net[30][100]
-// a)
+// a) express the address of the element net[22][56]
+// b) express the address of the element net[22][0] in two different ways
+// c) express the address of the element net[0][0] in three different ways
