913cfb89bb79f9a349a71b7b2aa53dfa82115eaa
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-18 23:59:20 +0200

    Add Chapter10 7th question solution

---
+++ CPrimerPlus/Chapter10/Questions/question10_7/question10_7.c
@@ -0,0 +1,15 @@
+// Form the declaration for each of the following variables:
+// a) digits is an array of ten integer values
+// b) bill is an array of six floating point variables
+// c) mate is an array of three arrays which contain five integer variables
+// d) wlanc in an pointer to an array with 20 elements
+// e) twz is an array of 20 pointers into char type
+#include <stdio.h>
+int main(void) {
+  int digits[10];
+  float bill[6];
+  int mate[3][5];
+  char (*wlanc)[20];
+  char * twz[20];
+  return 0;
+}
