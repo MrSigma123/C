6de69a96071c306d932deb3d41c788999ce66801
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-29 20:15:41 +0200

    Add Chapter10 6th question solution

--- CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
+++ CPrimerPlus/Chapter10/Questions/question10_6/question10_6.c
@@ -9,12 +9,18 @@ int main(void) {
   int net[30][100];
   int * p1;
   p1 = &net[22][56];
+  printf("%p\n", p1);
   int * p2;
   p2 = &net[22][0];
+  printf("%p\n", p2);
+  printf("%p\n", p2);
   p2 = net[22];
   int * p3;
   p3 = &net[0][0];
+  printf("%p\n", p3);
   p3 = net[0];
+  printf("%p\n", p3);
   p3 = *net;
+  printf("%p\n", p3);
   return 0;
 }
