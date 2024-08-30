10f2a83fbdb13a4f61b756139883f20197d4496a
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-30 13:40:26 +0200

    Add Chapter10 2nd exercise problem

--- CPrimerPlus/Chapter10/Exercises/exercise10_2/exercise10_2.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_2/exercise10_2.c
@@ -1,19 +1,19 @@
-/* 
-Write a program which initializes the array and then copies it's elements
-into two different arrays. (All three arrays should be declared in main
-program). To make the first copy of the array use the function which uses
-the offset notation. To make the second copy of the array use the function
-which uses the pointer notation along incrementin the pointers. The first
-two functions should be taking as the arguments the source array name,
-the destination array name and the arrays sizes. The third function should
-be taking the name of the source array, the name of the destination array
-and the pointer to the next element after the last element of the source
-array. The functions calls should look like below:
+/*
+Write a program which initializes an array and then copies its elements
+into two different arrays. (All three arrays should be declared in the main
+program). To make the first copy of the array, use a function that uses
+offset notation. To make the second copy of the array, use a function
+that uses pointer notation along with incrementing the pointers. The first
+two functions should take as arguments the source array name,
+the destination array name, and the array sizes. The third function should
+take the name of the source array, the name of the destination array,
+and a pointer to the element after the last element of the source
+array. The function calls should look like below:
 
 double source[5] = {1.1, 2.2, 3.3, 4.4, 5.5};
 double dest1[5];
 double dest2[5];
 copy_arr(source, dest1, 5);
-copy_ptr(source, dest1, 5);
-copy_ptr2(source, dest1, source + 5);
+copy_ptr(source, dest2, 5);
+copy_ptr2(source, dest2, source + 5);
 */
