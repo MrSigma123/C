0ab4283429423d53cf7d376760a428705cf8804d
MrSigma123 <karol.czolpinski@gmail.com>
2024-08-30 12:59:50 +0200

    Add Chapter10 1st exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_1/exercise10_1.c
@@ -0,0 +1,5 @@
+/*
+Modify the metereologic program from listing 10.7 is such way to make it
+perform calculations by using the pointers instead of indexes. (There is
+still necessary to declare and initialize the array)
+*/
