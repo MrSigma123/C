8126a44cae7645acae4b86f1cdd93afaec62928b
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-02 17:11:28 +0200

    Add Chapter10 3rd exercise solution

--- CPrimerPlus/Chapter10/Exercises/exercise10_3/exercise10_3.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_3/exercise10_3.c
@@ -2,4 +2,22 @@
 Write a function which returns the greatest value stored in the array.
 Test this function in a simple program.
 */
-
+#include <stdio.h>
+#define SIZE 10
+int find_greatest_val(int * arr, int size);
+int main(void)
+{
+  int array[] = {2,3,7,1,4,9,0,5,6,8};
+  printf("The greatest value in an array is %d", find_greatest_val(array, SIZE));
+  return 0;
+}
+int find_greatest_val(int * arr, int size)
+{
+  int i;
+  int greatest = arr[0];
+  for (i = 1; i < size; i++)
+  {
+    if (arr[i] > greatest) greatest = arr[i];
+  }
+  return greatest;
+}
