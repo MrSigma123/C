c95f411d11e5a0bbbfe7d521cbaf1747ef959fa8
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-03 23:59:02 +0200

    Add Chapter10 5th exercise solution

--- CPrimerPlus/Chapter10/Exercises/exercise10_5/exercise10_5.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_5/exercise10_5.c
@@ -3,3 +3,26 @@ Write a function which returns the difference between the greatest and
 the smallest elements stored in an array. Test that function in a simple
 program.
 */
+#include <stdio.h>
+#define SIZE 10
+int difference_btw_max_min(int * array, int size);
+int main(void)
+{
+  int array[SIZE] = {2,4,6,8,10,12,14,16,18,20};
+  printf("The difference between the max and min is %d.\n",
+         difference_btw_max_min(array, SIZE));
+  return 0;
+}
+int difference_btw_max_min(int * array, int size)
+{
+  int i, diff;
+  int max = array[0];
+  int min = array[0];
+  for (i = 1; i < size; i++)
+  {
+    if (array[i] < min) min = array[i];
+    if (array[i] > max) max = array[i];
+  }
+  diff = max - min;
+  return diff;
+}
