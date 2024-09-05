de09dfd20d1fda595f801211ec89036ce3f2640f
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-05 20:03:27 +0200

    Add Chapter10 9th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_9/exercise10_9.c
@@ -0,0 +1,8 @@
+/*
+Write a program which initialize two-dimensional array which size is 3*5 and
+which stores the double type elements and uses the VLA function to copy the
+data into another two-dimensional array. Write also another VLA using function
+which allows for displaying the content of the both arrays. The functions
+should operate on N*M arrays. (If your compiler doesn't allow for VLA arrays
+use instead traditional C programming ways and do the exercise for N*5 array).
+*/
