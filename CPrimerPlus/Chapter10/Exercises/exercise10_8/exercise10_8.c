3c2b3e8b29be435139eeed802f354f304177a0ff
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-05 19:54:03 +0200

    Add Chapter10 8th exercise solution

--- CPrimerPlus/Chapter10/Exercises/exercise10_8/exercise10_8.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_8/exercise10_8.c
@@ -7,3 +7,35 @@ what's necessary is choosing the right function arguments. (The arguments
 don't have to be the array name and its size—it's equally correct to pass
 the address of a specific element and the number of elements to be copied).
 */
+#include <stdio.h>
+void copy_arr(double * source, double * dest, int size);
+void print_array(double * pointer, int size);
+int main(void)
+{
+  double array[7] = {1,2,3,4,5,6,7};
+  double three_element_copy[3]; // uninitialized
+  copy_arr(array+2, three_element_copy, 3);
+  return 0;
+}
+void copy_arr(double * source, double * dest, int size)
+{
+  int i;
+  printf("Destination array before element swap:\n");
+  print_array(dest, size);
+  for (i = 0; i < size; i++)
+  {
+    dest[i] = source[i];
+  }
+  printf("\nDestination array after element swap:\n");
+  print_array(dest, size);
+}
+void print_array(double * pointer, int size)
+{
+  int i;
+  printf("Array elements: ");
+  for (i = 0; i < size; i++)
+  {
+    printf("%lf, ", *(pointer+i));
+  }
+  printf("\n");
+}
