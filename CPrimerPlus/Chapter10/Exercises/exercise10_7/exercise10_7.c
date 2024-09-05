0604ca9c862959c0986d2229abdadf7e62661709
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-05 19:13:57 +0200

    Add Chapter10 7th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_7/exercise10_7.c
@@ -0,0 +1,6 @@
+/* 
+Write a program that initializes a two-dimensional array and copies its
+elements into another two-dimensional array using the function from
+exercise10_2. (Since a two-dimensional array is an array of arrays,
+a one-dimensional copying function can be used for each subarray).
+*/
