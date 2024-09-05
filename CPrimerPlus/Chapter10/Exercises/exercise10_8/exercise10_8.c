2b4846dbc9cf7932811aa478fb562f25efbcd3bc
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-05 19:45:50 +0200

    Add Chapter10 8th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_8/exercise10_8.c
@@ -0,0 +1,9 @@
+/*
+Use the copying function from exercise10_2 to copy the following elements
+from an array. Copy the elements from the third to the fifth element of
+an array that has 7 elements. The elements should be placed into another
+three-element array. The function itself doesn't need to be modified;
+what's necessary is choosing the right function arguments. (The arguments
+don't have to be the array name and its size—it's equally correct to pass
+the address of a specific element and the number of elements to be copied).
+*/
