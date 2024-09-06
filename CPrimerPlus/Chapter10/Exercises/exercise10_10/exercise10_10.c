910b3b1a6370288c03cd62316cfdd0dc28a68af5
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-06 21:29:53 +0200

    Add Chapter10 10th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_10/exercise10_10.c
@@ -0,0 +1,7 @@
+/*
+Write a function which assigns its each element the sum of the values from
+the other two arrays. (If array1 has the values 2, 4, 5 and 8, and the array2
+has the values 1, 0, 4, and 6 the function will assign to the third array the
+values 3, 4, 9 and 14). The function should take as arguments three arrays
+names and their sizes. Try out this function in a simple program.
+*/
