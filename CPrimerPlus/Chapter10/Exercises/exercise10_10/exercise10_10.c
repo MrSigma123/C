8c4a87e5b9f0a71b17b4016ee14e177171d3d189
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-06 21:30:23 +0200

    Add Chapter10 10th exercise problem

--- CPrimerPlus/Chapter10/Exercises/exercise10_10/exercise10_10.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_10/exercise10_10.c
@@ -1,7 +1,8 @@
 /*
-Write a function which assigns its each element the sum of the values from
-the other two arrays. (If array1 has the values 2, 4, 5 and 8, and the array2
-has the values 1, 0, 4, and 6 the function will assign to the third array the
-values 3, 4, 9 and 14). The function should take as arguments three arrays
-names and their sizes. Try out this function in a simple program.
+Write a function that assigns to each element of the third array the sum of the 
+corresponding elements from two other arrays. (For example, if array1 has the 
+values 2, 4, 5, and 8, and array2 has the values 1, 0, 4, and 6, the function 
+should assign to the third array the values 3, 4, 9, and 14.) The function 
+should take three array names and their size as arguments. Test this function 
+in a simple program.
 */
