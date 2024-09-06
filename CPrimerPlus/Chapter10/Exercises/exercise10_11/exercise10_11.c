83164e45ed09f53896815187091cb04d21234ea1
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-06 22:02:59 +0200

    Add Chapter10 11th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_11/exercise10_11.c
@@ -0,0 +1,6 @@
+/*
+Write a program that declares a 3x5 integer array with some initial data, 
+displays the values, doubles the values, and then displays the new values. 
+Displaying and doubling the values should be executed by two separate 
+functions, which take the array name and its number of rows as arguments.
+*/
