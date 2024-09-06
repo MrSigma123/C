a40dd1e68898735f78f310231f1b5b43dbb1c8f2
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-06 23:33:27 +0200

    Add Chapter10 13th exercise problem

---
+++ CPrimerPlus/Chapter10/Exercises/exercise10_13/exercise10_13.c
@@ -0,0 +1,18 @@
+/*
+Write a program that asks the user about entering three sets which consists of
+the five double type values and then:
+a) Writes those data into 3*5 sized array
+b) Calculates the average for the whole separate set
+c) Calculates the average for all the values
+d) Finds the greates value from those 15 values
+e) Displays the results
+
+Each of the tasks should be executed by the separate function which use
+the standard C programming arrays approach. Complete the task b) by using
+the function which calculate and returs the average of the one-dimensional
+array. Use the loop to call it three times. The other tasks should take as
+argument the whole array and the exercises c) and d) should return the result
+to its calling program. You don't have to worry about verifying the input from
+the user - assume that the user won't make any mistakes while entering the
+data.
+*/
