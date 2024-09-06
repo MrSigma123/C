f2b3f062b5b9d62e40e5263ae8f6f68a0ed2210e
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-06 22:14:26 +0200

    Define printing and doubling functions

--- CPrimerPlus/Chapter10/Exercises/exercise10_11/exercise10_11.c
+++ CPrimerPlus/Chapter10/Exercises/exercise10_11/exercise10_11.c
@@ -4,3 +4,42 @@ displays the values, doubles the values, and then displays the new values.
 Displaying and doubling the values should be executed by two separate 
 functions, which take the array name and its number of rows as arguments.
 */
+#include <stdio.h>
+#define ROWS 3
+#define COLUMNS 5
+void display_2d_array(int array[][COLUMNS], int rows);
+void double_2d_array(int array[][COLUMNS], int rows);
+int main(void)
+{
+  int array[ROWS][COLUMNS] = {
+    {1,2,3,4,5},
+    {6,7,8,9,10},
+    {11,12,13,14,15}
+  };
+  return 0;
+}
+void display_2d_array(int array[][COLUMNS], int rows)
+{
+  int i, j;
+  for (i = 0; i < rows; i++)
+  {
+    for (j = 0; j < COLUMNS; j++)
+    {
+      if (i !=  - 1)
+        printf("%d, ", array[i][j]);
+      else
+        printf("%d", array[i][j]);
+    }
+  }
+}
+void double_2d_array(int array[][COLUMNS], int rows)
+{
+  int i, j;
+  for (i = 0; i < rows; i++)
+  {
+    for (j = 0; j < COLUMNS; j++)
+    {
+      array[i][j] *= 2;
+    }
+  }
+}
