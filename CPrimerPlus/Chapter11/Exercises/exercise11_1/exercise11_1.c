2fd34d8f13e8aa49c9145b98d7e9f7ef347dec77
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-19 16:36:42 +0200

    Add Chapter11 1st exercise problem

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_1/exercise11_1.c
@@ -0,0 +1,5 @@
+/* Design a function that takes the most recent n characters from the keyboard 
+   (including spaces, tabs, and newline characters) and writes the result into 
+   an array that is passed by reference as a function argument.
+*/
+
