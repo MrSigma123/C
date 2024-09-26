8b6c618058777e16b1e7088571f2ac42e02fc1a4
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-26 19:01:48 +0200

    Add Chapter11 2nd exercise problem

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_2/exercise11_2.c
@@ -0,0 +1,5 @@
+/*
+Modify the function from the exercise11_1 in such way to make it end its work
+after reading n characters or after detecting the occurrence of the first space
+character. (Don't use the scanf() function).
+*/
