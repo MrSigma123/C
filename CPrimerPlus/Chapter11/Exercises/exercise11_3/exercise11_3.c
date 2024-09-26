4a79b15d7fbfacd40d963cecc8d55ebea1fc15bb
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-26 19:18:33 +0200

    Add Chapter11 3rd exercise problem

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_3/exercise11_3.c
@@ -0,0 +1,7 @@
+/*
+Design a function which takes the data from the input and writes down the first
+word into the passed array, the rest of the string is omitted. The function
+should omit the space characters at the beginning of the input. You can count
+as a word the string of characters which doesn't include spaces, tabs or
+newline characters. Use the getchar() function.
+*/
