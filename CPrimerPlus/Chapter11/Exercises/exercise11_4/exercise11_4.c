93120a6072f9e1d919f0efebc456b0ff107905dc
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-28 23:45:45 +0200

    Add the code to be edited

--- CPrimerPlus/Chapter11/Exercises/exercise11_4/exercise11_4.c
+++ CPrimerPlus/Chapter11/Exercises/exercise11_4/exercise11_4.c
@@ -2,3 +2,45 @@
 Design and test the function from the exercise11_3, which additionally
 takes a limit on the input characters as a parameter.
 */
+// below is the code form the previous exercise
+/*
+Design a function that reads input and stores the first word into the passed 
+array, ignoring the rest of the string. The function should skip any leading 
+space characters. A word is defined as a sequence of characters that does not 
+include spaces, tabs, or newline characters. Use the getchar() function.
+*/
+#include <stdio.h>
+void get_first_word(char * word);
+int main(void)
+{
+  char first_word[81];
+  get_first_word(first_word);
+  printf("The first word is \"%s\"\n", first_word);
+  return 0;
+}
+void get_first_word(char * word)
+{
+  char ch;
+
+  printf("Enter the stream of characters:\n");
+  ch = getchar();
+
+  while (ch == '\n' || ch == '\t' || ch == ' ')
+  {
+    ch = getchar(); // skips any non printable characters
+  }
+  
+  while (ch != EOF && ch != '\n' && ch != '\t' && ch != ' ')
+  {
+    *word++ = ch;
+    ch = getchar();
+  }
+  
+  *word = '\0'; // close the string
+  
+    while (ch != '\n' && ch != EOF)
+  {
+    ch = getchar(); // skip all remaining characters
+  }
+}
+
