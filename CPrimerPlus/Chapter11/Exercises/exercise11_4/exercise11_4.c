c415da1f2ed3dc605559dca1357493bd471aff55
MrSigma123 <karol.czolpinski@gmail.com>
2024-09-29 00:10:49 +0200

    Add Chapter11 4th exercise solution

--- CPrimerPlus/Chapter11/Exercises/exercise11_4/exercise11_4.c
+++ CPrimerPlus/Chapter11/Exercises/exercise11_4/exercise11_4.c
@@ -10,17 +10,32 @@ space characters. A word is defined as a sequence of characters that does not
 include spaces, tabs, or newline characters. Use the getchar() function.
 */
 #include <stdio.h>
-void get_first_word(char * word);
+void get_first_word(char * word, int characters);
 int main(void)
 {
   char first_word[81];
-  get_first_word(first_word);
+  int chars;
+  
+  printf("How many characters do you want to read? Enter the value: ");
+  scanf("%d", &chars);
+  getchar(); // discard newline
+
+  while (chars <= 0 || chars >= 81)
+  {
+    printf("\nYou can enter from 1 to 80 characters.\n");
+    printf("Please enter again the number of characters to be read: ");
+    scanf("%d", &chars);
+    getchar(); // discard newline
+  }
+
+  get_first_word(first_word, chars);
   printf("The first word is \"%s\"\n", first_word);
   return 0;
 }
-void get_first_word(char * word)
+void get_first_word(char * word, int characters)
 {
   char ch;
+  int counter = 0;
 
   printf("Enter the stream of characters:\n");
   ch = getchar();
@@ -29,10 +44,12 @@ void get_first_word(char * word)
   {
     ch = getchar(); // skips any non printable characters
   }
-  
-  while (ch != EOF && ch != '\n' && ch != '\t' && ch != ' ')
+
+  while (ch != EOF && ch != '\n' && ch != '\t' && ch != ' '
+    && counter < characters)
   {
     *word++ = ch;
+    counter++;
     ch = getchar();
   }
   
