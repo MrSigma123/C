c386ea19a047f3c8c39c474e2bc8043df61ea4d6
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-21 13:16:06 +0200

    Add exercise11.5 solution

--- CPrimerPlus/Chapter11/Exercises/exercise11_5/exercise11_5.c
+++ CPrimerPlus/Chapter11/Exercises/exercise11_5/exercise11_5.c
@@ -13,43 +13,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-char * first_char_occurence(char * string, char target);
+char * first_char_occurrence(char * string, char target);
 
 int main(void){
+  int i;
   char string[81];
   char character;
   char * result;
 
   printf("The program will take a word and a test character in a loop\n");
-  printf("To stop the loop enter thre dots in a row (...).\n");
+  printf("To stop the loop enter just empty line [ENTER].\n");
  
   while(1){
     printf("\nEnter a word: ");
-    fgets(string, 80, stdin);
+    fgets(string, sizeof(string), stdin);
+    string[strcspn(string, "\n")] = '\0'; // remove '\0'
 
-    if(strcmp(string, "...") == 1){
+    if(string[0] == '\n' || string[0] == '\0'){
       printf("\nThe program ends here.\n");
       break;
     }
-    fflush(stdout);
 
     printf("Enter the character to be found: ");
-    scanf("%c", &character);
+    scanf(" %c", &character);
+    while (getchar() != '\n'); // clear the buffer
 
-    result = first_char_occurence(string, character);
+    result = first_char_occurrence(string, character);
     if (result == NULL){
-      printf("There is any occurecne of the \"%c\" character.\n",
+      printf("There is any occurrecne of the \"%c\" character.\n",
            character);
     }
     else{
-      printf("The first occurence of \"%c\" character is the "
+      printf("The first occurrence of \"%c\" character is the "
           "character under %p\n", character, result);
+      printf("The position of the occurrence is the character No.%ld.\n",
+             result - string + 1);
     }
   }
   return 0;
 }
 
-char * first_char_occurence(char * string, char target){
+char * first_char_occurrence(char * string, char target){
   int i;
   for (i=0; string[i] != '\0'; i++){
     if(string[i] == target){
