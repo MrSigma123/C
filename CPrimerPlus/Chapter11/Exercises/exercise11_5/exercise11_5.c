77efc1da822343922cdfc028a07cde74ef9c1be3
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-21 10:44:29 +0200

    Add exercise11.5 task

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_5/exercise11_5.c
@@ -0,0 +1,11 @@
+/*
+  Design a function which will find the first occurence of
+  certain character in the first passed argument. In case of
+  finding the the character the function should return the
+  pointer to that certain character. In onther case the function
+  should return the null value. (It's analogically to strchr()
+  function. Check that function by using it in simple program,
+  which will use a loop to chech several occurences and will pass
+  various data into tested function.
+*/
+
