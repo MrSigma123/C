f7850e64c2c1b51727ea4970cacfe25596e76528
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-24 13:23:56 +0200

    Add Chapter11 6th exercise question

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_6/exercise11_6.c
@@ -0,0 +1,8 @@
+/*
+  Write a function is_present_in(), which will take two arguments:
+  a character and the pointer to the string. The function should
+  return non zero value (true) if the string inclule passed
+  character. In other case the function should return zero (false).
+  Test the function in a simple program, which will test various
+  data in a loop.
+*/
