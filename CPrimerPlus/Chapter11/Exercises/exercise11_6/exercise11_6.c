bbfcd8b0f6070f217b161563a2bb4205ac6a24d8
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-24 13:34:34 +0200

    Add Chapter11 6th exercise program core

--- CPrimerPlus/Chapter11/Exercises/exercise11_6/exercise11_6.c
+++ CPrimerPlus/Chapter11/Exercises/exercise11_6/exercise11_6.c
@@ -6,3 +6,37 @@
   Test the function in a simple program, which will test various
   data in a loop.
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#define LIMIT 81
+
+int is_present_in(char target, char * arr_address);
+
+int main(void){
+  char string[LIMIT];
+  char test;
+
+  printf("The program will test various data in search of the"
+         "character presence in a string.\n");
+  printf("(type empty character [ENTER] in order to stop the program)\n");
+  while(1){
+    printf("Please enter the character to be tested: ");
+    scanf(" %c", &test);
+    printf("Please enter the string to be tested: ");
+    scanf(" %s", string);
+
+    if (is_present_in(test, string) == 1){
+      printf("The test character is present in the string.\n");
+    }
+    else{
+      printf("The test character is not present in the string.\n");
+    }
+  }
+  
+  return 0;
+}
+
+int is_present_in(char target, char * arr_address){
+  
+}
