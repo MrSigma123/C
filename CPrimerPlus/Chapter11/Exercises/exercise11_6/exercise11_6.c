43f7af4e3f9dcd5320043c8d18c9d4daf1113a87
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-24 14:42:16 +0200

    Add Chapter11 6th exercise solution

--- CPrimerPlus/Chapter11/Exercises/exercise11_6/exercise11_6.c
+++ CPrimerPlus/Chapter11/Exercises/exercise11_6/exercise11_6.c
@@ -17,15 +17,22 @@ int main(void){
   char string[LIMIT];
   char test;
 
-  printf("The program will test various data in search of the"
+  printf("The program will test various data in search of the "
          "character presence in a string.\n");
   printf("(type empty character [ENTER] in order to stop the program)\n");
+
   while(1){
-    printf("Please enter the character to be tested: ");
-    scanf(" %c", &test);
+    printf("\nPlease enter the character to be tested: ");
+   // if(test == '\n'){
+   //   getc(stdin); // ommit '\n' from previous loop cycles
+   // };
+    test = getc(stdin); 
+    if (test == '\n'){
+      break;
+    }
     printf("Please enter the string to be tested: ");
     scanf(" %s", string);
-
+    getc(stdin); // get remaining '\n'
     if (is_present_in(test, string) == 1){
       printf("The test character is present in the string.\n");
     }
@@ -38,5 +45,11 @@ int main(void){
 }
 
 int is_present_in(char target, char * arr_address){
-  
+  int i;
+  for (i=0; *(arr_address+i)!='\0'; i++){
+    if(*(arr_address+i) == target){
+      return 1;
+    }
+  }
+  return 0; // in case of not finding the test character
 }
