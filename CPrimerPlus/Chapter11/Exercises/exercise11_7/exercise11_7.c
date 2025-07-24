3278783919ed5de9f3f488a9985b5ac8def7417a
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-24 14:47:22 +0200

    Add Chapter11 7th exercise question

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_7/exercise11_7.c
@@ -0,0 +1,9 @@
+/*
+  Function strncpy(s1, s2, n) copies from s2 to s1 exactly n
+  characters, in case of exceedung the character amount by
+  shortening s2 string or adding to it '\0' characters. The result
+  string can end with other character than '\0' if s2 length is n
+  or more. The return value is s1. Write your own implementation
+  of that function.
+*/
+
