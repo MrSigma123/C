f412beb1c7bc2fc884d3d71f7f7e59a3f5c896c6
MrSigma123 <karol.czolpinski@gmail.com>
2025-07-28 16:55:31 +0200

    Add Chapter11 8th exercise question

---
+++ CPrimerPlus/Chapter11/Exercises/exercise11_8/exercise11_8.c
@@ -0,0 +1,9 @@
+/*
+  Write a function include_str(), which takes as arguments
+  two string pointers. If second string is present inside first
+  the function should return the address where that string is
+  present. In example calling include_str("pula", "ul") should
+  return the address of the letter u in the word "pula". If
+  second string is not present inside the first one the return
+  value should be NULL.
+*/
